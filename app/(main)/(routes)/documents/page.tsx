0f49a0fd92df76b46520dd58bd0a3acce30c7448
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-10-21 16:12:45 -0400

    Added a dropdown to show user details

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,37 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { useUser } from "@clerk/clerk-react";
+import { PlusCircle } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 
 const DocumentsPage = () => {
-  return <div>This a protected route</div>;
+  const { user } = useUser();
+  console.log({ user });
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        alt="empty"
+        width={300}
+        height={300}
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        alt="empty"
+        width={300}
+        height={300}
+        className="hidden dark:block"
+      />
+      <h2>Welcome to {user?.username}&apos;s Jotion</h2>
+      <Button>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+  );
 };
 
 export default DocumentsPage;
